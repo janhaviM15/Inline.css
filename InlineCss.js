ee308d7fec29d5f6eb8b2ee591f25c32e875d748
Janhavi Mali <167394492+janhaviM15@users.noreply.github.com>
2025-08-16 10:37:47 +0530

    Add files via upload

---
+++ InlineCss.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Css()
+{
+    return(
+        <>
+        <h1 style={{color:"orange"}}>This is an exple for Inline styling!!!</h1>
+        <p style={{backgroundColor:"aqua"}}>Paragraph starts here </p>
+        </>
+    );
+
+}
+export default Css;
